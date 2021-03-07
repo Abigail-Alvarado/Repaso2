de7459c5b47108f6f80f0b08dc651d644a00f4a0
Abigail Gaitan <abi49386307@outlook.com>
2021-03-07 17:09:13 -0600

    Clases

---
+++ Repaso2/Clases/vehiculos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso2.Clases
+{
+    class vehiculos
+    {
+        string placa;
+        String marca;
+        string modelo;
+        string color;
+        float precioK;
+
+        public string Placa { get => placa; set => placa = value; }
+        public string Marca { get => marca; set => marca = value; }
+        public string Modelo { get => modelo; set => modelo = value; }
+        public string Color { get => color; set => color = value; }
+        public float PrecioK { get => precioK; set => precioK = value; }
+    }
+}
--- Repaso2/Formularios/Form1.Designer.cs
+++ Repaso2/Formularios/Form1.Designer.cs
@@ -41,7 +41,7 @@ namespace Repaso2
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(287, 376);
+            this.button1.Location = new System.Drawing.Point(309, 376);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(203, 42);
             this.button1.TabIndex = 0;
@@ -54,7 +54,7 @@ namespace Repaso2
             this.button2.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.Location = new System.Drawing.Point(29, 376);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(203, 42);
+            this.button2.Size = new System.Drawing.Size(222, 42);
             this.button2.TabIndex = 1;
             this.button2.Text = "Registro Vehiculos";
             this.button2.UseVisualStyleBackColor = true;
@@ -63,7 +63,7 @@ namespace Repaso2
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(527, 376);
+            this.button3.Location = new System.Drawing.Point(543, 376);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(203, 42);
             this.button3.TabIndex = 2;
@@ -80,15 +80,15 @@ namespace Repaso2
             this.panel1.Controls.Add(this.button2);
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(771, 447);
+            this.panel1.Size = new System.Drawing.Size(811, 447);
             this.panel1.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
-            this.label1.Location = new System.Drawing.Point(129, 9);
+            this.label1.Location = new System.Drawing.Point(154, 23);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(513, 338);
+            this.label1.Size = new System.Drawing.Size(513, 325);
             this.label1.TabIndex = 3;
             this.label1.Text = " ";
             // 
@@ -96,7 +96,7 @@ namespace Repaso2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(762, 459);
+            this.ClientSize = new System.Drawing.Size(809, 431);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
