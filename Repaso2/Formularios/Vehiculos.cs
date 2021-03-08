7c5011df2db6f161cdcaba4d1a904578d522a36b
Abigail Gaitan <abi49386307@outlook.com>
2021-03-07 19:27:39 -0600

    Registros

--- Repaso2/Formularios/Vehiculos.cs
+++ Repaso2/Formularios/Vehiculos.cs
@@ -7,30 +7,81 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Repaso2
 {
     public partial class Vehiculos : Form
     {
-        List<Vehículos> vehiculos = new List<Vehículos>();
-        string archivo2 = "vehículos.txt";
+        List<VehiculosH> vehiculos = new List<VehiculosH>();
+        VehiculosH cliv = new VehiculosH();
+        string archivoV = "Registrovehículos.txt";
         public Vehiculos()
         {
             InitializeComponent();
         }
+        public void guardar()
+        {
+            FileStream stream = new FileStream(archivoV, FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(stream);
+
+            for (int i = 0; i < vehiculos.Count; i++)
+            {
+                writer.WriteLine(vehiculos[i].Placa);
+                writer.WriteLine(vehiculos[i].Marca);
+                writer.WriteLine(vehiculos[i].Modelo);
+                writer.WriteLine(vehiculos[i].Color);
+                writer.WriteLine(vehiculos[i].PrecioK);
+            }
+            writer.Close();
+        }
+        void leer_datos()
+        {
+            FileStream stream = new FileStream(archivoV, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+            while (reader.Peek() > -1)
+            {
+                VehiculosH tempvehiculos = new VehiculosH();
+                tempvehiculos.Placa = reader.ReadLine();
+                tempvehiculos.Marca = reader.ReadLine();
+                tempvehiculos.Modelo = reader.ReadLine();
+                tempvehiculos.Color = reader.ReadLine();
+                tempvehiculos.PrecioK = float.Parse(reader.ReadLine());
+                vehiculos.Add(tempvehiculos);
+                reader.Close();
+            }
+        }
+            void limpiar()
+            {
+                textBox1.Text = null;
+                textBox2.Text = null;
+                textBox3.Text = null;
+                comboBox1.Text = null;
+                textBox4.Text = null;
+            }
+        private int Repeticion(string placa) // retorna 1 si se encuentra en la lista
+        {
+            int resultado = 0;
+            for (int i = 0; i < vehiculos.Count; i++)
+            {
+                if (vehiculos[i].Placa.Contains(placa))
+                    resultado = 1;
+            }
+            return resultado;
+        }
 
         private void button1_Click(object sender, EventArgs e)
-        {
-            Form1 vclientes = new Form1();
-            vclientes.Show();
-            this.SetVisibleCore(false);
-        }
+            {
+                Form1 vclientes = new Form1();
+                vclientes.Show();
+                this.SetVisibleCore(false);
+            }
 
-        
 
-        private void button2_Click(object sender, EventArgs e)
-        {
 
+            private void button2_Click(object sender, EventArgs e)
+            {
+
+            }
         }
     }
-}
--- Repaso2/VehiculosH.cs
+++ Repaso2/VehiculosH.cs
@@ -10,5 +10,25 @@ namespace Repaso2
     {
         string placa, marca, modelo, color;
         float precioK;
+
+        public string Placa { get => placa; set => placa = value; }
+        public string Marca { get => marca; set => marca = value; }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // VehiculosH
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.ClientSize = new System.Drawing.Size(579, 397);
+            this.Name = "VehiculosH";
+            this.ResumeLayout(false);
+
+        }
+
+        public string Modelo { get => modelo; set => modelo = value; }
+        public string Color { get => color; set => color = value; }
+        public float PrecioK { get => precioK; set => precioK = value; }
     }
 }
